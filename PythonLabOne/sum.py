7f245dde2850522c792e1e66fa71da0a91f6e495
Samana Shrestha <samanashrestha02@gmail.com>
2021-05-31 09:42:15 +0545

    sum of three

---
+++ PythonLabOne/sum.py
@@ -0,0 +1,7 @@
+''' This program that takes three numbers and prints its sum'''
+num1=int(input("enter first number"))
+num2=int(input("enter second num"))
+num3=int(input("enter third num"))
+sum=num1+num2+num3
+print(sum)
+
