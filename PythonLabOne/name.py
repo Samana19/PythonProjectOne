dd298d290e006126b0f598498f3e670d00d89ac6
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-10 14:46:18 +0545

    desk ra students

---
+++ PythonLabOne/name.py
@@ -0,0 +1,5 @@
+name=input('Kimi no namaye nandesuka')
+age=int(input('Enter your age'))
+print("My name is", name, "and iam", age,"years old")
+print(f'My name is {name} and iam {age} years old')
+print("My name is"  +name+ "and iam"  +str(age)+  "years old")
