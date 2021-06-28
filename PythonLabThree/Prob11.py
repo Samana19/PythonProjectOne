91799fe4b91231c6203949cbc022d58e3fbd2069
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-28 10:25:44 +0545

    factorial of a num

--- PythonLabThree/Prob11.py
+++ PythonLabThree/Prob11.py
@@ -1,1 +1,8 @@
 '''Write a program to find the factorial of a number using functions.'''
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Enter a number : "))
+print(factorial(n))
