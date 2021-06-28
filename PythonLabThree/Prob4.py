9abb1dad9bb8d7a81ee2ce9cf28fd71e3a07f670
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-28 09:57:55 +0545

    sum of multiples of 3 and 5

--- PythonLabThree/Prob4.py
+++ PythonLabThree/Prob4.py
@@ -1,2 +1,8 @@
-'''Write a function that returns the sum of multiples of 3 and 5 between 0 andlimit(parameter).
-For example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20'''
+'''Write a function that returns the sum of multiples of 3 and 5 between 0 and limit(parameter)'''
+def sum_of_multiples(limit):
+    summ=0
+    for i in range(1,limit+1):
+        if i%3 or i%5==0:
+            summ=+i
+    print(summ)
+sum_of_multiples(20)
