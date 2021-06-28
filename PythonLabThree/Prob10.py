d37e115547f67599714deeda0824ad325607b5d5
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-28 10:21:05 +0545

    palindrome

--- PythonLabThree/Prob10.py
+++ PythonLabThree/Prob10.py
@@ -1,1 +1,9 @@
 '''Accept string from the user and display only those characters which are present at an even index'''
+def string(str):
+  result = ""
+  for i in range(len(str)):
+    if i % 2 == 0:
+      result = result + str[i]
+  return result
+
+print(string('abcdef'))
