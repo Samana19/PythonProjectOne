0288b1fec04b5ea3b96a07bc1180cc2f102126ff
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-28 10:04:47 +0545

    sum of multiples of 3 and 5

--- PythonLabThree/Prob7.py
+++ PythonLabThree/Prob7.py
@@ -1,1 +1,7 @@
 '''Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.'''
+def string(s):
+    up = sum(1 for i in s if i.isupper())
+    low = sum(1 for i in s if i.islower())
+    print( "No. of Upper case characters : %s,No. of Lower case characters : %s" % (up,low))
+
+string("HeLlO WoRld")
