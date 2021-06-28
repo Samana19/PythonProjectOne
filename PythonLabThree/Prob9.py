74ca5e8c800f8044d54d973b86b82796db214779
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-28 10:15:17 +0545

    palindrome

--- PythonLabThree/Prob9.py
+++ PythonLabThree/Prob9.py
@@ -1,1 +1,13 @@
 '''Write a Python function that checks whether a passed string is palindrome or not.'''
+def palindrome(a):
+    left_pos = 0
+    right_pos = len(a) - 1
+
+    while right_pos >= left_pos:
+        if not a[left_pos] == a[right_pos]:
+            return False
+        left_pos += 1
+        right_pos -= 1
+    return True
+
+print(palindrome('12121'))
