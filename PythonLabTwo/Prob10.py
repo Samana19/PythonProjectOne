0b610741e1cc9de5a7de38fdff6b7b0fc9651d82
Samana Shrestha <samanashrestha02@gmail.com>
2021-06-30 12:13:47 +0545

    sum of three if two int same then zero

--- PythonLabTwo/Prob10.py
+++ PythonLabTwo/Prob10.py
@@ -1,1 +1,9 @@
 ''' Write a Python program to sum of three given integers. However, if two values are equal sum will be zero'''
+def sum(a , b, c):
+    if a == b or b == c or a==c:
+        sum = 0
+    else:
+        sum = a + b + c
+    return sum
+
+print(sum(3, 1, 2))
